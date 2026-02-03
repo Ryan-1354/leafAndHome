49241b68cc27da6d1a7845840d320d0eb03cb3d3
002 <tourrec002@gmail.com>
2026-02-03 12:07:38 +0800

    basic layout done

--- src/pages/front/Cart.jsx
+++ src/pages/front/Cart.jsx
@@ -26,7 +26,7 @@ function Cart() {
     <>
       <div className="container mb-5 ">
         <div className="row d-flex justify-content-between">
-          <div className="col-9 ">
+          <div className="col-9">
             <div className="cartSection border mb-4">
               <div className="head d-flex justify-content-between py-5 px-5 bg-secondary bg-opacity-25">
                 <h3>購物車</h3>
@@ -70,33 +70,130 @@ function Cart() {
                 </tbody>
               </table>
             </div>
-            <div className="addOnSection border">
-              <div className="head d-flex justifify-contetn-start align-items-end">
+            <div className="addOnSection border mb-4">
+              <div className="head d-flex justiffy-contetn-start align-items-end">
                 <h3>加購服務</h3>
                 <h5>常一起選購的加購服務</h5>
               </div>
               <ul>
                 <li>
                   <img src="" alt="" />
-                  <div className="content d-flex flex-column justify-content-start">
-                    <h5>到貨換盆</h5>
-                    <h6>專業換盆服務</h6>
-                    <div className="last d-flex justify-content-between ">
-                      <input type="number" defaultValue={1} />
-                      <div className="cta d-flex justify-content-end align-items-center">
-                        <h4>NT$150</h4>
-                        <button type="button">加入</button>
+                  <div className="content d-flex justify-content-start">
+                    <img
+                      src="https://static.vecteezy.com/system/resources/thumbnails/008/695/917/small/no-image-available-icon-simple-two-colors-template-for-no-image-or-picture-coming-soon-and-placeholder-illustration-isolated-on-white-background-vector.jpg"
+                      alt=""
+                      style={{ height: "100px" }}
+                    />
+                    <div className="content d-flex flex-column justify-content-start w-100">
+                      <h5>到貨換盆</h5>
+                      <h6>專業換盆服務</h6>
+                      <div className="last d-flex justify-content-between w-100">
+                        <input type="number" defaultValue={1} />
+                        <div className="cta d-flex justify-content-end align-items-center">
+                          <h4>NT$150</h4>
+                          <button type="button">加入</button>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </li>
-                <li></li>
-                <li></li>
+                <li>
+                  <img src="" alt="" />
+                  <div className="content d-flex justify-content-start">
+                    <img
+                      src="https://static.vecteezy.com/system/resources/thumbnails/008/695/917/small/no-image-available-icon-simple-two-colors-template-for-no-image-or-picture-coming-soon-and-placeholder-illustration-isolated-on-white-background-vector.jpg"
+                      alt=""
+                      style={{ height: "100px" }}
+                    />
+                    <div className="content d-flex flex-column justify-content-start w-100">
+                      <h5>到貨換盆</h5>
+                      <h6>專業換盆服務</h6>
+                      <div className="last d-flex justify-content-between w-100">
+                        <input type="number" defaultValue={1} />
+                        <div className="cta d-flex justify-content-end align-items-center">
+                          <h4>NT$150</h4>
+                          <button type="button">加入</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <img src="" alt="" />
+                  <div className="content d-flex justify-content-start">
+                    <img
+                      src="https://static.vecteezy.com/system/resources/thumbnails/008/695/917/small/no-image-available-icon-simple-two-colors-template-for-no-image-or-picture-coming-soon-and-placeholder-illustration-isolated-on-white-background-vector.jpg"
+                      alt=""
+                      style={{ height: "100px" }}
+                    />
+                    <div className="content d-flex flex-column justify-content-start w-100">
+                      <h5>到貨換盆</h5>
+                      <h6>專業換盆服務</h6>
+                      <div className="last d-flex justify-content-between w-100">
+                        <input type="number" defaultValue={1} />
+                        <div className="cta d-flex justify-content-end align-items-center">
+                          <h4>NT$150</h4>
+                          <button type="button">加入</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div className="saved border">
+              <div className="head d-flex justiffy-contetn-start align-items-end">
+                <h3>收藏清單</h3>
+                <h5>那些您曾停下來看過的植物</h5>
+              </div>
+              <ul>
+                <li>
+                  <img src="" alt="" />
+                  <div className="content d-flex justify-content-start">
+                    <img
+                      src="https://static.vecteezy.com/system/resources/thumbnails/008/695/917/small/no-image-available-icon-simple-two-colors-template-for-no-image-or-picture-coming-soon-and-placeholder-illustration-isolated-on-white-background-vector.jpg"
+                      alt=""
+                      style={{ height: "100px" }}
+                    />
+                    <div className="content d-flex flex-column justify-content-start w-100">
+                      <h5>吊蘭</h5>
+                      <h6>Spider Plant</h6>
+                      <div className="last d-flex justify-content-between w-100">
+                        <input type="number" defaultValue={1} />
+                        <div className="cta d-flex justify-content-end align-items-center">
+                          <h4>NT$150</h4>
+                          <button type="button">加入</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <img src="" alt="" />
+                  <div className="content d-flex justify-content-start">
+                    <img
+                      src="https://static.vecteezy.com/system/resources/thumbnails/008/695/917/small/no-image-available-icon-simple-two-colors-template-for-no-image-or-picture-coming-soon-and-placeholder-illustration-isolated-on-white-background-vector.jpg"
+                      alt=""
+                      style={{ height: "100px" }}
+                    />
+                    <div className="content d-flex flex-column justify-content-start w-100">
+                      <h5>波士頓蕨</h5>
+                      <h6>Boston Fern</h6>
+                      <div className="last d-flex justify-content-between w-100">
+                        <input type="number" defaultValue={1} />
+                        <div className="cta d-flex justify-content-end align-items-center">
+                          <h4>NT$150</h4>
+                          <button type="button">加入</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
               </ul>
             </div>
           </div>
           <div className="col-3">
-            <div className="card border px-5 py-5 position-sticky">
+            <div className="card border px-5 py-5 position-sticky top-0">
               <div className="card-head">
                 <h3 className="card-title text-center">訂單內容</h3>
               </div>
